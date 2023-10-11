a2c68aa0143562212feac549a25a0db5e3c2716f
Juanjo11114 <jpicon15@unab.edu.co>
2023-10-11 17:52:09 -0500

    adelanto_clase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,8 @@ dependencies {
     implementation("de.hdodenhof:circleimageview:3.0.1")
     //Picasso para poder mostrar imagenes de internet
     implementation("com.squareup.picasso:picasso:2.8")
+
+    // implementación de lottie splash screen
+    implementation ("com.airbnb.android:lottie:6.1.0")
 }
 
--- app/src/main/java/com/fabiansuarez/mywallet/SplashActivity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/SplashActivity.java
@@ -2,7 +2,9 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent myIntent = new Intent(SplashActivity.this, CategoryActivity.class);
+                startActivity(myIntent);
+                finish();
+
+
+            }
+        }, 5000);
     }
 }
